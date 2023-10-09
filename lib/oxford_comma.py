10bb0a66256ed74cf5cd9e87a9dd2f32365a377a
wkimunya <136619749+wkimunya@users.noreply.github.com>
2023-10-09 14:22:00 +0300

    Complete lab

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,10 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 0:
+        return ""
+    elif len(items) == 1:
+        return items[0]
+    elif len(items) == 2:
+        return f"{items[0]} and {items[1]}"
+    else:
+        comma_elements = ", ".join(items[:-1])
+        return f"{comma_elements}, and {items[-1]}"
